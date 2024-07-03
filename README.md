bc796365e6cc73fd4d8b299688cd7c00fd7672a1
Ron Dear <rdear@kent.edu>
2024-07-02 23:20:41 -0400

    Added basic db connection and connection close functionality

--- README.md
+++ README.md
@@ -11,3 +11,15 @@
 - [x] Add argparser
 - [x] Add cProfiler
 - [x] Add Logging
+
+### v0.2
+- [x] Add Create DB
+- [ ] Add DB Table for directories
+- [ ] Add DB Table for mediaFiles
+- [ ] Add Drop DB Tables ALL
+- [ ] Add Media Finder (Simple)
+
+    #### Notes
+    The "simple" version of the media finder will ONLY be the recusive function that searches all directories for other directories
+
+    Create 
--- main.py
+++ main.py
@@ -3,15 +3,32 @@ import cProfile
 import logging.config
 import logging.handlers
 import pstats
+import sqlite3
+
+LOGGER_NAME = "PHOTOS_MANAGER"
+DB_NAME = "photos_manager.db"
 
 parser = argparse.ArgumentParser()
 # logging.basicConfig(filename="test.log", level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 def setupArgparser():
+    
+    logger.debug("Setting up argparser..")
+
     parser.add_argument("-t", "--test", help="This is only a test argument", action="store_true")
     parser.add_argument("-p", "--profile", help="Run the script with the profiler turned on", action="store_true")
+    parser.add_argument("-db", "--setupdb", help="Create the database and the tables for directories and mediaFiles", action="store_true")
+    parser.add_argument("-ddb", "--dropdb", help="Drop all the tables in the database", action="store_true")
 
-    return parser.parse_args()
+    
+
+    try:
+        args = parser.parse_args()
+        logger.debug("Attempted to parse args")
+        logger.debug("Argparser setup complete")
+        return args
+    except Exception as e:
+        logger.exception(f'Error parsing args: {e}')
 
 def setupLogger():
 
@@ -28,8 +45,8 @@ def setupLogger():
             "stderr": {
                 "class": "logging.StreamHandler",
                 "formatter": "simple",
-                "stream": "ext://sys.stderr",
-                "level": "WARNING"
+                "stream": "ext://sys.stdout",
+                "level": "DEBUG"
             },
             "file": {
                 "class": "logging.handlers.RotatingFileHandler",
@@ -46,22 +63,53 @@ def setupLogger():
 
     }
 
-    logger = logging.getLogger("photos_manager")
+    logger = logging.getLogger(LOGGER_NAME)
 
     logging.config.dictConfig(logging_config)
 
     return logger
 
+def createDirectoriesTable(conn):
+    pass
+
+def setupDatabase():
+
+    logger.debug("Connecting to database")
+    connection = None
+
+    try:
+        connection = sqlite3.connect(DB_NAME)
+    except Exception as e:
+        logger.exception("Unable to connect to the database")
+
+
+    # logger.debug("Creating directories table")
+    return connection
+
+def closeDBConnection(conn):
+    logger.debug("Closing connection to db")
+
+    try:
+        conn.close()
+        logger.debug("DB connection closed")
+    except Exception as e:
+        logger.exception(f'Unable to close the connection to the db: {e}')
+
 if __name__ == "__main__":
+
+    conn = None
 
     with cProfile.Profile() as profile:
 
         logger = setupLogger()
 
         logger.debug("********************************************************")
-        logger.debug("Setting up argparser..")
-        args = setupArgparser()
-        logger.debug("Argparser setup complete")
+        args = setupArgparser()        
+
+        if args.setupdb:
+
+            conn = setupDatabase()
+            
 
     if args.profile:
         results = pstats.Stats(profile)
@@ -69,7 +117,10 @@ if __name__ == "__main__":
         results.print_stats()
         results.dump_stats("results.prof")
     
-
+    if conn:
+        closeDBConnection(conn)
+    else:
+        logger.debug("DB connection not opened")
 
 
 
