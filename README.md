79a5717dbea6e8b53fd83c68447c434f6f53074a
Ron Dear <rdear@kent.edu>
2024-08-18 21:24:43 -0400

    Added functions for processing PNG and videos. Added check for .env indicating DEV_MODE to automatically drop tables when running.

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 
 ## Features
 
+## Dependencies
+
+    #### System
+    - ffmpeg
+
+    #### Python packages
+    - ffmpeg-python
+    - PIL
+
 
 ## Versions
 ---
@@ -23,6 +32,18 @@
     The "simple" version of the media finder will ONLY be the recusive function that searches all directories for other directories
 
 ### v0.3
-- [ ] Create dict with media info
-        - Filetype, image data hash, exif data
-- [ ] Insert data into DB
+- [x] Create dict with media info
+        - Filepath, filetype, creation date, image data hash, exif data (if available)
+- [x] Insert data into DB
+- [ ] Add support for MP4
+- [x] Add support for M4V
+- [x] Add support for MOV
+- [x] Add support for JPG, JPEG, PNG
+
+    #### Notes
+    With this version, the process media function takes a list of filepaths of all media files that were found. If it's an image, it will load the image using PIL, hash the image, extract EXIF data (where available), return the dict with that info, and finally write that info to a database.
+
+    Function for processing videos not working properly. Video location data is stored as LATLON (eg. +31.0982-82.1335). Currently the function splits on "-". Need to add regex to find the two parts of the LAT LON data.
+
+
+    
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import os
 from dotenv import load_dotenv, dotenv_values
 from PIL import Image, ExifTags
 import hashlib
+import datetime
+import ffmpeg
 
 
 TABLE_DEFS = [{
@@ -17,11 +19,12 @@ TABLE_DEFS = [{
     id INTEGER PRIMARY KEY,
     FILENAME TEXT NOT NULL,
     FILEPATH TEXT NOT NULL UNIQUE,
-    HASH TEXT NOT NULL,
+    HASH TEXT,
     FILETYPE TEXT NOT NULL,
-    DATETIME TEXT NOT NULL,
-    LATITUDE TEXT,
-    LONGITUDE TEXT
+    DATETIME,
+    LATITUDE,
+    LONGITUDE,
+    PROCESSING_COMPLETE INTEGER
     );
     '''
 }]
@@ -113,7 +116,7 @@ def setupLogger(LEVEL):
                 "level": LEVEL,
                 "formatter": "simple",
                 "filename": "photos_manager.log",
-                "maxBytes": 10000,
+                "maxBytes": 100000,
                 "backupCount": 3
             }
         },
@@ -191,76 +194,118 @@ def findMedia(target_dir):
     return files
 
 def insertDataIntoDB(imgData, dbConn):
-
-    logger.info(f"Adding info for image: {imgData['filename']} to db")
+    logger.debug(f"Adding info for image: {imgData['filename']} to db")
     curr = dbConn.cursor()
     try:
-        curr.execute(f"INSERT INTO media (FILENAME, FILEPATH, HASH, FILETYPE, DATETIME, LATITUDE, LONGITUDE) VALUES ('{imgData['filename']}', '{imgData['filepath']}', '{imgData['hash']}', '{imgData['filetype']}', '{imgData['date']}', '{imgData['lat']}', '{imgData['lon']}')")
+        curr.execute(f"INSERT INTO media (FILENAME, FILEPATH, HASH, FILETYPE, DATETIME, LATITUDE, LONGITUDE, PROCESSING_COMPLETE) VALUES ('{imgData['filename']}', '{imgData['filepath']}', '{imgData['hash']}', '{imgData['filetype']}', '{imgData['date']}', '{imgData['lat']}', '{imgData['lon']}', '{imgData['processing_complete']}')")
         dbConn.commit()
     except Exception as e:
         logger.exception(f"Failed to insert data for image: {imgData['filepath']} into db")
+        logger.exception(imgData)
         logger.exception(e)
 
+def processJPG(_imgData):
 
-def processJPG(filepath):
-
-    img = Image.open(filepath)
+    img = Image.open(_imgData["filepath"])
     exifData = {
             ExifTags.TAGS[k]: v
             for k, v, in img._getexif().items()
             if k in ExifTags.TAGS
         }
 
-    
-    imgData = {
-        "hash": "",
-        "filename": filepath.split("/")[-1],
-        "filetype": filepath.split(".")[-1],
-        "filepath": filepath,
-        "date": "",
-        "lat": "",
-        "lon": ""
-    }
-
     try:
-        imgData = {
-            "hash": hashlib.sha256(img.tobytes()).hexdigest(),
-            "filename": filepath.split("/")[-1],
-            "filetype": filepath.split(".")[-1],
-            "filepath": filepath,
-            "date": "" if exifData.get("DateTime", None) is None else exifData["DateTime"],
-            "lat": 0 if exifData.get("GPSInfo", None) is None else round(float(exifData["GPSInfo"][2][0] + exifData["GPSInfo"][2][1]/60 + exifData["GPSInfo"][2][2]/3600), 6),
-            "lon": 0 if exifData.get("GPSInfo", None) is None else round(float(exifData["GPSInfo"][4][0] + exifData["GPSInfo"][4][1]/60 + exifData["GPSInfo"][4][2]/3600), 6),
-        }
+        _imgData["hash"] = hashlib.sha256(img.tobytes()).hexdigest()
+        _imgData["date"] = datetime.datetime.fromtimestamp(os.stat(_imgData["filepath"]).st_birthtime) if exifData.get("DateTime", None) is None else exifData["DateTime"]
+        _imgData["lat"] = 0 if exifData.get("GPSInfo", None) is None else round(float(exifData["GPSInfo"][2][0] + exifData["GPSInfo"][2][1]/60 + exifData["GPSInfo"][2][2]/3600), 6)
+        _imgData["lon"] = 0 if exifData.get("GPSInfo", None) is None else round(float(exifData["GPSInfo"][4][0] + exifData["GPSInfo"][4][1]/60 + exifData["GPSInfo"][4][2]/3600), 6)
+        _imgData["processing_complete"] = 1
     except Exception as e:
-        logger.exception(f"Failed to process file at path: {filepath}")
+        logger.exception(f"Failed to process file at path: {_imgData['filepath']}")
         logger.exception(e)
 
-    return imgData
+    return _imgData
 
-PROCESSORS = [(["jpg", "jpeg"], processJPG)]
+def processPNG(_imgData):
+
+    img = Image.open(_imgData["filepath"])
+    # exifData = {
+    #         ExifTags.TAGS[k]: v
+    #         for k, v, in img._getexif().items()
+    #         if k in ExifTags.TAGS
+    #     }
+
+    try:
+        _imgData["hash"] = hashlib.sha256(img.tobytes()).hexdigest()
+        _imgData["date"] = datetime.datetime.fromtimestamp(os.stat(_imgData["filepath"]).st_birthtime)
+        _imgData["lat"] = 0 
+        _imgData["lon"] = 0
+        _imgData["processing_complete"] = 1
+    except Exception as e:
+        logger.exception(f"Failed to process file at path: {_imgData['filepath']}")
+        logger.exception(e)
+
+    return _imgData
+
+def processVideo(_imgData):
+
+    metaData = ffmpeg.probe(_imgData["filepath"])
+    location = metaData.get("format", {}).get("tags").get("location")
+    try:
+        _imgData["hash"] = ""
+        _imgData["date"] = 0 if metaData.get("format", {}).get("tags").get("date") is None else metaData.get("format", {}).get("tags").get("date")
+        _imgData["lat"] = 0 if location is None else location.split("-")[0]
+        _imgData["lon"] = 0 if location is None else location.split("-")[1]
+        _imgData["processing_complete"] = 1
+    except Exception as e:
+        logger.exception(f"Failed to process file at path: {_imgData['filepath']}")
+        logger.exception(e)
+
+    return _imgData
+
+def processAAE(_imgData):
+
+    _imgData["processing_complete"] = 1
+
+    return _imgData
+
+PROCESSORS = [(["jpg", "jpeg"], processJPG), (["png"], processPNG), (["m4v", "mov"], processVideo), (["AAE"], processAAE)]
 
 def processMedia(files, dbConn):
 
     fileTypes = []
     
     # for filepath in ["test_dir/IMG_3277.JPG"]:
+    
     for filepath in files:
         logger.info(f"Processing file: {filepath}")
         extension = filepath.split(".")[-1]
 
-        if extension not in fileTypes:
-            fileTypes.append(extension)
+        if extension.lower() not in fileTypes:
+            fileTypes.append(extension.lower())
 
+        logger.info(f"File {filepath} created at: {datetime.datetime.fromtimestamp(os.stat(filepath).st_birthtime)}")
+        imgData = {
+            "filename": filepath.split("/")[-1],
+            "filetype": filepath.split(".")[-1].lower(),
+            "filepath": filepath,
+            "hash": "",
+            "lat": "",
+            "lon": "",
+            "date": "",
+            "processing_complete": 0
+        }
         for processer in PROCESSORS:
 
             if extension.lower() in processer[0]:
-                imgData = processer[1](filepath)
-                # logger.info(imgData)
-                insertDataIntoDB(imgData, dbConn)
+                imgData = processer[1](imgData)
+                break
+        
+        insertDataIntoDB(imgData, dbConn)
+        
 
 
 
+    logger.info(f'{len(files)} files were processed')
     logger.info(f"{len(fileTypes)} file types were found")
     for ext in fileTypes:
         logger.info(f"\t{ext}")
@@ -274,16 +319,21 @@ if __name__ == "__main__":
         args = setupArgparser()
 
         logger = setupLogger("CRITICAL" if args.critical else "INFO")
-                
 
         #setup connection to DB and check to see if the proper tables exist
         conn = connectToDB()
+
+        if os.getenv("DEV_MODE"):
+            logger.info("DEV_MODE is True. Clearing db...")
+            dropTables(conn)
+
+        #ensure the proper db tables exists. If not, create them
         checkIfTablesExists(conn)
     
         if args.find:
             filepaths = findMedia(args.path)
 
-            processMedia(filepaths, conn)    
+            processMedia(filepaths, conn)
 
         if args.dropdb:
             dropTables(conn)
